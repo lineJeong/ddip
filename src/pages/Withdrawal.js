1d6ee9e7a270384b7c08f3a5e6a47850deb5302c
lineJeong <line.jeong32@gmail.com>
2023-05-13 22:34:00 +0900

    feat: 회원 탈퇴 페이지 마크업 작업

--- src/pages/Withdrawal.js
+++ src/pages/Withdrawal.js
@@ -1,7 +1,15 @@
+import HeadingPageContent from "../components/PageContent/HeadingPageContent";
 import RootPageContent from "../components/PageContent/RootPageContent";
+import WithdrawalForm from "../components/Withdrawal/WithdrawalForm";
 
 function WithdrawalPage() {
-  return <RootPageContent maxWidth="sm">WithdrawalPage</RootPageContent>;
+  return (
+    <RootPageContent maxWidth="sm">
+      <HeadingPageContent heading="회원 탈퇴">
+        <WithdrawalForm />
+      </HeadingPageContent>
+    </RootPageContent>
+  );
 }
 
 export default WithdrawalPage;
