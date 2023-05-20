ddb4c1bddfe203c47e9f06e47eae8e8da6299991
lineJeong <line.jeong32@gmail.com>
2023-05-20 23:08:27 +0900

    feat: 유효성 검사 함수를 인수로 받는 useValidInput 커스텀 훅 작업

---
+++ src/@hooks/useValidInput.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const useValidInput = (validate, defaultValue = "") => {
+  const [value, setValue] = useState(defaultValue);
+  const [isTouched, setIsTouched] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const errorMsg = validate(value);
+  const isValid = errorMsg.length === 0;
+  const hasError = !isValid && isTouched;
+
+  useEffect(() => {
+    setErrorMessage(errorMsg);
+  }, [errorMsg]);
+
+  const handleChangeInput = (event) => {
+    setValue(event.target.value);
+  };
+  const handleBlurInput = () => {
+    setIsTouched(true);
+  };
+  const handleReset = () => {
+    setValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value,
+    isValid,
+    hasError,
+    errorMessage,
+    handleChangeInput,
+    handleBlurInput,
+    handleReset
+  };
+};
+
+export default useValidInput;
