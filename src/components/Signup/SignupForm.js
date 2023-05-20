bda09f4ed2539fe5b63d3560c32e66e3122c1695
lineJeong <line.jeong32@gmail.com>
2023-05-20 23:12:05 +0900

    feat: 회원가입 기능 구현

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import styled from "styled-components";
 
+import useAxios from "../../@hooks/useAxios";
+import useUniqueNickname from "../../@hooks/useUniqueNickname";
+import useValidInput from "../../@hooks/useValidInput";
+import * as validationUtil from "../../@utils/validation";
 import Button from "../UI/Button";
-import ErrorMsg from "../UI/ErrorMsg";
 import Input from "../UI/Input";
 import InputWithLabel from "../UI/InputWithLabel";
+import ValidMessage from "../UI/ValidMessage";
 
 const InputWrapper = styled.div`
   margin-bottom: 16px;
@@ -11,6 +17,92 @@ const InputWrapper = styled.div`
 `;
 
 function SignupForm() {
+  const navigate = useNavigate();
+
+  const {
+    value: email,
+    isValid: isEmailValid,
+    hasError: hasEmailError,
+    errorMessage: emailErrorMessage,
+    handleChangeInput: handleChangeEmail,
+    handleBlurInput: handleBlurEmail
+  } = useValidInput(validationUtil.isValidEmail);
+  const {
+    value: nickname,
+    isValid: isNicknameValid,
+    hasError: hasNicknameError,
+    errorMessage: nicknameErrorMessage,
+    handleChangeInput: handleChangeNickname,
+    handleBlurInput: handleBlurNickname
+  } = useValidInput(validationUtil.isValidNickname);
+  const {
+    value: password,
+    isValid: isPasswordValid,
+    hasError: hasPasswordError,
+    errorMessage: passwordErrorMessage,
+    handleChangeInput: handleChangePassword,
+    handleBlurInput: handleBlurPassword
+  } = useValidInput(validationUtil.isValidPassword);
+  const {
+    value: confirmPassword,
+    isValid: isConfirmPasswordValid,
+    hasError: hasConfirmPasswordError,
+    errorMessage: confirmPasswordErrorMessage,
+    handleChangeInput: handleChangeConfirmPassword,
+    handleBlurInput: handleBlurConfirmPassword
+  } = useValidInput(validationUtil.isValidConfirmPassword.bind(null, password));
+
+  const {
+    isNicknameUnique,
+    validMessage,
+    hasValidMessage,
+    handleCheckNickname,
+    isLoading: isNicknameLoading
+  } = useUniqueNickname(nickname, isNicknameValid);
+
+  const combinedPasswordErrorMessage = validationUtil.combineTwoErrorMessage(
+    hasPasswordError,
+    passwordErrorMessage,
+    hasConfirmPasswordError,
+    confirmPasswordErrorMessage
+  );
+
+  let isFormValid = false;
+  if (
+    isEmailValid &&
+    isNicknameValid &&
+    isPasswordValid &&
+    isConfirmPasswordValid &&
+    isNicknameUnique
+  ) {
+    isFormValid = true;
+  }
+
+  const onSuccess = () => {
+    navigate("/email-auth", {
+      state: { email, isEmailValid }
+    });
+  };
+
+  const { error, isLoading, sendRequest } = useAxios(onSuccess);
+
+  // 추후 이모지도 랜덤으로 전송해야 함!!
+  const handleSubmit = () => {
+    if (!isFormValid || isLoading) return;
+
+    const config = {
+      method: "POST",
+      url: "/auth/signup",
+      data: {
+        email,
+        nickname,
+        password
+      }
+    };
+    sendRequest(config);
+    console.error(error);
+  };
+
   return (
     <>
       <InputWrapper>
@@ -19,8 +111,13 @@ function SignupForm() {
           id="email-signup"
           type="email"
           placeholder="이메일"
+          value={email}
+          onChange={handleChangeEmail}
+          onBlur={handleBlurEmail}
         />
-        <ErrorMsg hasError>이메일을 입력해주세요.</ErrorMsg>
+        <ValidMessage hasValidMessage={hasEmailError}>
+          {emailErrorMessage}
+        </ValidMessage>
       </InputWrapper>
       <InputWrapper>
         <InputWithLabel
@@ -28,11 +125,30 @@ function SignupForm() {
           id="nickname-signup"
           placeholder="닉네임"
           radius="top"
+          value={nickname}
+          onChange={handleChangeNickname}
+          onBlur={handleBlurNickname}
         />
-        <Button background="white" radius="bottom" size="sm" outline fullWidth>
+        <Button
+          background="white"
+          radius="bottom"
+          size="sm"
+          outline
+          fullWidth
+          onClick={handleCheckNickname}
+          disabled={isNicknameLoading || hasNicknameError}
+        >
           닉네임 중복 확인
         </Button>
-        <ErrorMsg hasError>닉네임을 입력해주세요.</ErrorMsg>
+        <ValidMessage hasValidMessage={hasNicknameError}>
+          {nicknameErrorMessage}
+        </ValidMessage>
+        <ValidMessage
+          hasValidMessage={hasValidMessage}
+          success={isNicknameUnique}
+        >
+          {validMessage}
+        </ValidMessage>
       </InputWrapper>
       <InputWrapper>
         <InputWithLabel
@@ -41,16 +157,32 @@ function SignupForm() {
           type="password"
           placeholder="비밀번호"
           radius="top"
+          value={password}
+          onChange={handleChangePassword}
+          onBlur={handleBlurPassword}
         />
         <Input
           id="repassword-signup"
           type="password"
           placeholder="비밀번호 확인"
           radius="bottom"
+          value={confirmPassword}
+          onChange={handleChangeConfirmPassword}
+          onBlur={handleBlurConfirmPassword}
         />
-        <ErrorMsg hasError>비밀번호를 입력해주세요.</ErrorMsg>
+        <ValidMessage
+          hasValidMessage={hasPasswordError || hasConfirmPasswordError}
+        >
+          {combinedPasswordErrorMessage}
+        </ValidMessage>
       </InputWrapper>
-      <Button background="mainViolet" color="white" fullWidth>
+      <Button
+        background="mainViolet"
+        color="white"
+        onClick={handleSubmit}
+        disabled={!isFormValid || isLoading}
+        fullWidth
+      >
         이메일 인증하러 가기
       </Button>
     </>
