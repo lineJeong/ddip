108cf637b308d14ea1770032b6c35deb277c49d0
lineJeong <line.jeong32@gmail.com>
2023-05-21 14:37:56 +0900

    refactor: 에러메시지 결합 함수 리팩토링 및 전체 조기 반환 함수로 리팩토링

--- src/@utils/validation.js
+++ src/@utils/validation.js
@@ -1,4 +1,6 @@
-export const isValidEmail = (email) => {
+import { particlesMatching } from "../@constants/particlesMatching";
+
+export const validateEmail = (email) => {
   const pattern =
     /^(?!.*[\s<>])[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   if (!pattern.test(email)) {
@@ -7,57 +9,68 @@ export const isValidEmail = (email) => {
   return "";
 };
 
-export const isValidNickname = (nickname) => {
+export const validateNickname = (nickname) => {
   const pattern = /^[a-zA-Z0-9ㄱ-힣]{2,10}$/;
-  let errorMessage = "";
 
   if (!pattern.test(nickname)) {
     if (nickname.length < 2 || nickname.length > 10) {
-      errorMessage = "닉네임은 2자 이상 10자 이하여야 합니다.";
+      return "닉네임은 2자 이상 10자 이하여야 합니다.";
     } else {
-      errorMessage = "닉네임은 영문, 숫자, 한글만 사용할 수 있습니다.";
+      return "닉네임은 영문, 숫자, 한글만 사용할 수 있습니다.";
     }
   }
-  return errorMessage;
+  return "";
 };
 
-export const isValidPassword = (password) => {
+export const validatePassword = (password) => {
   const pattern =
     /^(?=.*[A-Za-z])(?=.*\d)(?!.*[ㄱ-ㅎㅏ-ㅣ가-힣])[A-Za-z\d]{8,}$/;
 
-  let errorMessage = "";
-
   if (!pattern.test(password)) {
     if (password.length < 8) {
-      errorMessage = "비밀번호는 최소 8자리 이상이어야 합니다.";
-    } else if (!/(?=.*[A-Za-z])/.test(password)) {
-      errorMessage = "비밀번호는 최소한 하나의 영문자를 포함해야 합니다.";
-    } else if (!/(?=.*\d)/.test(password)) {
-      errorMessage = "비밀번호는 최소한 하나의 숫자를 포함해야 합니다.";
-    } else if (/.*[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(password)) {
-      errorMessage = "비밀번호에는 한글이 포함될 수 없습니다.";
+      return "비밀번호는 최소 8자 이상이어야 합니다.";
+    }
+    if (!/(?=.*[A-Za-z])/.test(password)) {
+      return "비밀번호에는 최소한 하나의 영문자가 포함되어야 합니다.";
+    }
+    if (!/(?=.*\d)/.test(password)) {
+      return "비밀번호에는 최소한 하나의 숫자가 포함되어야 합니다.";
+    }
+    if (/.*[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(password)) {
+      return "비밀번호에는 한글이 포함될 수 없습니다.";
+    }
+    if (/.*\s/.test(password)) {
+      return "비밀번호에는 공백이 포함될 수 없습니다.";
     }
   }
-  return errorMessage;
+
+  return "";
 };
 
-export const isValidConfirmPassword = (confirmPassword, password) => {
-  if (confirmPassword !== password) {
+export const validateConfirmPassword = (password, confirmPassword) => {
+  if (password !== confirmPassword) {
     return "비밀번호가 일치하지 않습니다.";
   }
   return "";
 };
 
-export const combineTwoErrorMessage = (
-  hasFirstError,
-  firstErrorMessage,
-  hasSecondError,
-  secondErrorMessage
-) => {
-  const hasOnlySecondError = !hasFirstError && hasSecondError;
-  let errorMessage = firstErrorMessage;
-  if (hasOnlySecondError) {
-    errorMessage = secondErrorMessage;
+export const validateNotEmpty = (label, value) => {
+  if (!value.trim().length > 0) {
+    const particle = particlesMatching[label] || "을(를)";
+    return `${label}${particle} 입력해주세요.`;
   }
-  return errorMessage;
+  return "";
+};
+
+export const combineErrorMessages = (...errorMessages) => {
+  const validErrorMessages = errorMessages.filter(
+    (errorMessage) =>
+      typeof errorMessage === "string" && errorMessage.trim() !== ""
+  );
+
+  if (validErrorMessages.length === 0) {
+    return "";
+  }
+
+  return validErrorMessages[0];
 };
